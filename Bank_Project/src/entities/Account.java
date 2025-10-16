2b3c7979f88285d6cfc24730cfa44356fc9ecc41
Gabriel Soares <104767123+GabrielSoaresz@users.noreply.github.com>
2025-10-16 18:46:15 -0300

    Bank creation

---
+++ Bank_Project/src/entities/Account.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Account {
+	private int number;
+	private String holder;
+	protected double balance;
+	
+	public Account() {
+		
+	}
+
+	public Account(int number, String holder, double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		double totalTransaction = amount + 5;
+		if(balance >= totalTransaction) {
+			balance -= totalTransaction;
+			System.out.println("Withdrawal of $" + amount + " successfully completed!");
+		}
+		else {
+			System.out.println("Insufficient balance.");
+		}
+	}
+}
